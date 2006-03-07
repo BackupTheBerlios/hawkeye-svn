4abdc92a9da2b6dbf2884a9388b7731644d242e4
nebulon <nebulon@1c1a4fcd-a109-0410-a88b-be5c1930b859>
2006-03-07 22:00:08 +0000

    Let's remove everything

--- trunk/Core/HEDataType.py
+++
@@ -1,3 +0,0 @@
-HEContainer = "container"
-HEFile = "file"
-HELink = "link"
--- trunk/Core/HEException.py
+++
@@ -1,7 +0,0 @@
-class HEException(Exception):
-
-	def __init__(self, msg):
-		self.__msg = msg
-		
-	def __str__(self):
-		return self.__msg
